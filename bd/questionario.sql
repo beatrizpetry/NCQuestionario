56df20f20fc7c0b21476862522ee8444553b0d0d
Bia <beatriz.petry@outlook.com>
2023-10-25 15:47:42 -0300

    arrumando pasta

---
+++ bd/questionario.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Questionário (
+	Pergunta VARCHAR(200),
+    Situação VARCHAR(15) NOT NULL,
+    Descrição_nc VARCHAR(300),
+    Responsável_nc VARCHAR(60),
+    Classificação_nc VARCHAR(30),
+    Resolvido_nc VARCHAR(20)
+);
